f85d7d927743c4ef00b3b297ab27e00f3794ad4b
ovgofman <o.v.gofman@gmail.com>
2017-06-01 20:17:10 +0300

    update 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Hillel3 on 01.06.2017.
+ */
+public class Main {
+   
+
+    public static void main(String[] args){
+
+        List<Integer> list = new test().test();
+        System.out.println(list.get(0));
+
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class test {
+    public List<Integer> test(){
+        List<Integer> list = new LinkedList<>();
+        list.add(123);
+        System.out.println(list.get(0));
+        return list;
+    }
+}
