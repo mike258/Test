c12af935e58510dc8a25bbdcd7836258591b504b
ovgofman <o.v.gofman@gmail.com>
2017-06-01 20:17:30 +0300

    update 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import java.util.LinkedList;
+
 import java.util.List;
 
-/**
- * Created by Hillel3 on 01.06.2017.
- */
 public class Main {
-   
+
 
     public static void main(String[] args){
 
